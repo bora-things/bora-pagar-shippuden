c838072e775dec45b1c34352d7795768bd83b8b4
José Eduardo Santos <mds.josee@gmail.com>
2025-02-05 22:38:15 -0300

    refactor: update tests

--- backend/src/test/java/com/borathings/borapagar/user/UserControllerTests.java
+++ backend/src/test/java/com/borathings/borapagar/user/UserControllerTests.java
@@ -4,50 +4,25 @@ import static org.springframework.security.test.web.servlet.request.SecurityMock
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureWebMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.context.TestConfiguration;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 @WebMvcTest(UserController.class)
+@Import(UserMapperImpl.class)
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 @AutoConfigureWebMvc
 public class UserControllerTests {
     @Autowired
     private MockMvc mockMvc;
 
-    @Autowired
-    private UserMapper userMapper;
-
-    @MockBean
+    @MockitoBean
     private UserService userService;
-
-    @TestConfiguration
-    static class TestConfig {
-        @Bean
-        UserMapper userMapper() {
-            return new UserMapperImpl();
-        }
-    }
-
-    private UserEntity user;
-
-    @BeforeEach
-    public void setUp() {
-
-        user = UserEntity.builder()
-                .id(1L)
-                .name("Isaac")
-                .email("isaac.lourenco.704@ufrn.edu.br")
-                .imageUrl("https://lindao.com")
-                .build();
-    }
 
     @Test
     public void shouldGetAuthenticatedUser() throws Exception {
