dfcc86f13c938f0d5d8621df62b6567fb58a49a7
MatheusSCristo <cristo.matheus@outlook.com>
2025-02-11 15:05:49 -0300

    test: Add IndexEnumTest

---
+++ backend/src/test/java/com/borathings/borapagar/student/index/IndexEnumTest.java
@@ -0,0 +1,21 @@
+package com.borathings.borapagar.student.index;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class IndexEnumTest {
+
+    @Test
+    void testFromIdValidIndex() {
+        IndexEnum index = IndexEnum.fromId(1); // ID existente
+        Assertions.assertEquals(IndexEnum.MC, index);
+    }
+
+    @Test
+    void testFromIdInvalidIndex() {
+        int invalidId = 999; // ID inexistente
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            IndexEnum.fromId(invalidId);
+        }, "Deveria lançar exceção para ID inválido.");
+    }
+}
