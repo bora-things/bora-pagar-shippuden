230f6909a3ad26fa23accc910e1e2b0214bf1cc0
MatheusSCristo <cristo.matheus@outlook.com>
2025-08-16 15:49:04 -0300

    refact(workload): remove unnecessary attributes

--- src/main/java/com/borathings/borapagar/workload/WorkloadDto.java
+++ src/main/java/com/borathings/borapagar/workload/WorkloadDto.java
@@ -5,7 +5,4 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 public record WorkloadDto(
         @JsonProperty("ch-total-integralizada") Integer totalWorkloadCompleted,
         @JsonProperty("ch-total-minima") Integer totalMinimumWorkload,
-        @JsonProperty("ch-total-pendente") Integer pendingWorkload,
-        @JsonProperty("id-curso") Integer idCurso,
-        @JsonProperty("id-discente") Integer idDiscente,
-        @JsonProperty("sigla-nivel") String siglaNivel) {}
+        @JsonProperty("ch-total-pendente") Integer pendingWorkload) {}
--- src/test/java/com/borathings/borapagar/workload/WorkloadDtoTests.java
+++ src/test/java/com/borathings/borapagar/workload/WorkloadDtoTests.java
@@ -8,13 +8,10 @@ public class WorkloadDtoTests {
 
     @Test
     public void testWorkloadDtoCreation() {
-        WorkloadDto dto = new WorkloadDto(50, 30, 20, 1, 1001, "GRAD");
+        WorkloadDto dto = new WorkloadDto(50, 30, 20);
 
         assertThat(dto.totalWorkloadCompleted()).isEqualTo(50);
         assertThat(dto.totalMinimumWorkload()).isEqualTo(30);
         assertThat(dto.pendingWorkload()).isEqualTo(20);
-        assertThat(dto.idCurso()).isEqualTo(1);
-        assertThat(dto.idDiscente()).isEqualTo(1001);
-        assertThat(dto.siglaNivel()).isEqualTo("GRAD");
     }
 }
