04b782b586bef8b1b436d3166caff78d0e1af809
MatheusSCristo <cristo.matheus@outlook.com>
2025-08-20 22:33:27 -0300

    refact(classroom): remove unused attributes on schema

--- src/main/resources/db/migration/V1.20250210202755__CREATE_CLASSROOMS_TABLE.sql
+++ src/main/resources/db/migration/V1.20250210202755__CREATE_CLASSROOMS_TABLE.sql
@@ -3,22 +3,12 @@ CREATE TABLE IF NOT EXISTS classrooms
     id                       BIGSERIAL PRIMARY KEY,
     classroom_id             BIGINT  UNIQUE,
     year                     INTEGER ,
-    student_capacity         INTEGER ,
     component_code           VARCHAR(255) ,
     classroom_code           VARCHAR(255) ,
-    schedule_description     TEXT,
-    external_teacher_id      BIGINT,
-    education_mode_id        BIGINT,
-    classroom_status_id      BIGINT,
-    grouping_classroom_id    BIGINT,
     unit_id                  BIGINT,
     location                 TEXT,
     component_name           TEXT,
     period                 INTEGER ,
-    level_abbreviation       VARCHAR(100),
-    subgroup                 BOOLEAN ,
-    type                     INTEGER ,
-    uses_new_virtual_classroom BOOLEAN ,
     student_id               BIGINT,
     docent_id               BIGINT,
     component_id               BIGINT,
@@ -27,4 +17,5 @@ CREATE TABLE IF NOT EXISTS classrooms
     deleted_at               TIMESTAMPTZ,
     CONSTRAINT fk_classroom_student FOREIGN KEY (student_id) REFERENCES students(id),
     CONSTRAINT fk_classroom_subject FOREIGN KEY (component_id) REFERENCES components(id)
+
 );
